a48689981cc102d562f7e3edc4a9d69dec8f5fe6
Chennupati Balu <57926813+CHENNUPATIBALU@users.noreply.github.com>
2022-05-02 16:17:26 +0530

    Initial commit

---
+++ src/main/java/com/tekshop/ProductEntry.java
@@ -0,0 +1,38 @@
+package com.tekshop;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class ProductEntry extends HttpServlet{
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		String category = req.getParameter("category");
+		String prodName = req.getParameter("prod-name");
+		String doa = req.getParameter("doa");
+		String invLvl = req.getParameter("inv-lvl");
+		String shortDesc = req.getParameter("short-desc");
+		String longDesc = req.getParameter("long-desc");
+		String perUnitPrice = req.getParameter("per-unit-price");
+		String discount = req.getParameter("dis-range");
+		String yesRadio = req.getParameter("y-radio");
+		String noRadio = req.getParameter("n-radio");
+		
+		ProductEntryDAO ped;
+		try {
+			ped = new ProductEntryDAO(prodName, longDesc, "", "AppBel", "", category, category, "",Integer.parseInt(perUnitPrice), Integer.parseInt(invLvl), "",0,"");
+			ped.addProduct();
+			PrintWriter out = res.getWriter();
+			out.write("<script>alert('Success');</script>");
+					
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/main/java/com/tekshop/ProductEntryDAO.java
@@ -0,0 +1,86 @@
+package com.tekshop;
+import java.sql.*;
+
+public class ProductEntryDAO {
+	
+	static Connection con;
+	static String productId;
+	static String productName;
+	static String productDesc;
+	static String productFeatures;
+	static String productManufacturer;
+	static String productSize;
+	static String productCategory;
+	static String productSubCategory;
+	static String productUnit;
+	static int unitPrice;
+	static int inventoryLevel;
+	static String priceCurrency;
+	static int inventoryThreshold;
+	//static Blob image;
+	static String image;
+	
+	public ProductEntryDAO(String productName, String productDesc, String productFeatures, 
+							String productManufacturer,
+							String productSize,
+							String productCategory,
+							String productSubCategory,
+							String productUnit,
+							int unitPrice,
+							int inventoryLevel,
+							String priceCurrency,
+							int inventoryThreshold,
+							String image) throws Exception {
+		ProductEntryDAO.image = image;
+		ProductEntryDAO.inventoryLevel = inventoryLevel;
+		ProductEntryDAO.inventoryThreshold = inventoryThreshold;
+		ProductEntryDAO.priceCurrency = priceCurrency;
+		ProductEntryDAO.productCategory = productCategory;
+		ProductEntryDAO.productDesc = productDesc;
+		ProductEntryDAO.productFeatures = productFeatures;
+		ProductEntryDAO.productManufacturer = productManufacturer;
+		ProductEntryDAO.productName = productName;
+		ProductEntryDAO.productSize = productSize;
+		ProductEntryDAO.productSubCategory = productSubCategory;
+		ProductEntryDAO.productUnit = productUnit;
+		ProductEntryDAO.unitPrice = unitPrice;
+		ProductEntryDAO.makeConnection("com.mysql.cj.jdbc.Driver", "jdbc:mysql://localhost:3306/tekshop", "root", "b@/uv!n@y7474");
+	}
+	
+	public static void makeConnection(String driverType, String driverUrl, String userName, String password) throws Exception {
+		Class.forName(driverType);
+		con = DriverManager.getConnection(driverUrl,userName,password);
+		System.out.println("Connection success");
+	}
+	
+	public void addProduct() throws Exception {
+		productId = getGeneratedProductId();
+		String query = "insert into products values(?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+		System.out.println(productId);
+		String values[] = {productId,productName,productDesc,productFeatures,productManufacturer,productSize,productCategory,productSubCategory,productUnit,inventoryLevel+"",unitPrice+"",priceCurrency,inventoryThreshold+"",/*String.valueOf(image.getBinaryStream())*/""};
+		PreparedStatement ps = con.prepareStatement(query, values);
+		ps.setString(1, productId);
+		ps.setString(2, productName);
+		ps.setString(3, productDesc);
+		ps.setString(4, productFeatures);
+		ps.setString(5, productManufacturer);
+		ps.setString(6, productSize);
+		ps.setString(7, productCategory);
+		ps.setString(8, productSubCategory);
+		ps.setString(9, productUnit);
+		ps.setString(10, inventoryLevel+"");
+		ps.setString(11, unitPrice+"");
+		ps.setString(12, priceCurrency);
+		ps.setString(13, inventoryThreshold+"");
+		ps.setString(14, "");
+		ps.execute();
+	}
+	
+	public static String getGeneratedProductId() {
+		return generateProductId();
+	}
+	
+	public static String generateProductId() {
+		return String.format("%s%s",productCategory.substring(0, 3),productName.substring(0, 3));
+	}
+}
