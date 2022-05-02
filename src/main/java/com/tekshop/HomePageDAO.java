5c6040e28e4715a5aa78ec04aa75ed7f964b0d3f
Chennupati Balu <57926813+CHENNUPATIBALU@users.noreply.github.com>
2022-05-02 18:48:17 +0530

    HomePage updated
    - Products are getting displayed.

--- src/main/java/com/tekshop/HomePageDAO.java
+++ src/main/java/com/tekshop/HomePageDAO.java
@@ -21,23 +21,26 @@ public class HomePageDAO {
 		
 		while(rs.next()) {
 			HashMap<String,String> map = new HashMap<String, String>();
-			map.put("Product_ID", rs.getString(0));
-			map.put("Product_Name", rs.getString(1));
-			map.put("Product_Desc", rs.getString(2));
-			map.put("Product_Features", rs.getString(3));
-			map.put("Product_Manufacturer", rs.getString(4));
-			map.put("Product_size", rs.getString(5));
-			map.put("Product_Category", rs.getString(6));
-			map.put("Product_Sub_Category", rs.getString(7));
-			map.put("Product_Unit", rs.getString(8));
-			map.put("Product_Inventory", rs.getString(9));
-			map.put("Product_Unit_Price", rs.getString(10));
-			map.put("Product_Price_Currency", rs.getString(11));
-			map.put("Product_Inventory_Threshold", rs.getString(12));
-			map.put("Product_Image", rs.getString(13));
+			map.put("Product_ID", rs.getString(1));
+			map.put("Product_Name", rs.getString(2));
+			map.put("Product_Desc", rs.getString(3));
+			map.put("Product_Features", rs.getString(4));
+			map.put("Product_Manufacturer", rs.getString(5));
+			map.put("Product_size", rs.getString(6));
+			map.put("Product_Category", rs.getString(7));
+			map.put("Product_Sub_Category", rs.getString(8));
+			map.put("Product_Unit", rs.getString(9));
+			map.put("Product_Inventory", rs.getString(10));
+			map.put("Product_Unit_Price", rs.getString(11));
+			map.put("Product_Price_Currency", rs.getString(12));
+			map.put("Product_Inventory_Threshold", rs.getString(13));
+			map.put("Product_Image", rs.getString(14));
 			
 			products.add(map);
 		}
+		
+		con.close();
+		st.close();
 		
 		return products;
 	}
